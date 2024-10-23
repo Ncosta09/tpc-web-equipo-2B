1ab13529ee1e9c260bbbe65af402353bcef048a3
AgustinDeDonato <agusdedonato@hotmail.com>
2024-10-22 21:40:56 -0300

    Implementacion Vista MenuPersonas + Carga en Code Behind

---
+++ TPCWinForm_equipo-2B/TPC-Resto/MenuPersonas.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Menu Personas" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeBehind="MenuPersonas.aspx.cs" Inherits="TPC_Resto.MenuPersonas" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container mt-5">
+        <h2 class="text-center mb-4">Listado de Personas</h2>
+        <asp:GridView ID="gvUsuarios" runat="server" AutoGenerateColumns="false" CssClass="table table-striped">
+            <Columns>
+                <asp:BoundField DataField="Nombre" HeaderText="Nombre" />
+                <asp:BoundField DataField="Apellido" HeaderText="Apellido" />
+                <asp:BoundField DataField="Rol" HeaderText="Rol" />
+                <asp:BoundField DataField="DNI" HeaderText="DNI" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:Button ID="btnEliminar" runat="server" Text="Eliminar" CommandArgument='<%# Eval("IdUsuario") %>' CssClass="btn btn-danger" OnClick="btnEliminar_Click" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ TPCWinForm_equipo-2B/TPC-Resto/MenuPersonas.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using Negocio;
+
+namespace TPC_Resto
+{
+    public partial class MenuPersonas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarUsuarios();
+            }
+        }
+
+        private void CargarUsuarios()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                // Consulta SQL para obtener los usuarios y sus roles
+                string consulta = @"SELECT U.IdUsuario, U.Nombre, U.Apellido, R.Descripcion AS Rol, U.DNI
+                                    FROM Usuarios U
+                                    INNER JOIN Roles R ON U.IdRol = R.IdRol";
+
+                datos.setConsulta(consulta);
+                datos.ejecutarLectura();
+
+                // Enlaza el lector con el GridView
+                gvUsuarios.DataSource = datos.Lector;
+                gvUsuarios.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                Response.Write("<script>alert('Error al cargar usuarios: " + ex.Message + "');</script>");
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int idUsuario = Convert.ToInt32(btn.CommandArgument);
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                // Consulta SQL para eliminar el usuario por su ID
+                string consulta = "DELETE FROM Usuarios WHERE IdUsuario = @IdUsuario";
+                datos.setConsulta(consulta);
+                datos.setParametro("@IdUsuario", idUsuario);
+                datos.ejecutarAccion();
+
+                // Recargar el listado de usuarios después de la eliminación
+                CargarUsuarios();
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                Response.Write("<script>alert('Error al eliminar usuario: " + ex.Message + "');</script>");
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
